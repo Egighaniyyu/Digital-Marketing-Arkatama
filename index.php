e9b7d967384908d9faffc0d2cd8865f660033e32
egighaniyyu <egidgm123@gmail.com>
2022-06-13 13:09:42 +0700

    update section baru

---
+++ index.php
@@ -0,0 +1,14 @@
+//make function to show "zero" if last number is zero
+$num = 100;
+$last_num = .$num[strlen($num)-1];
+
+echo $last_num;
+
+<!-- function showZero($num){
+if($num == 0){
+return "zero";
+}
+else{
+return $num;
+}
+} -->
